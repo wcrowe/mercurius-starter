5d234b96b4a585ad1996671fedb9d7bc4c55d9a2
wcrowe <crowe.will@gmail.com>
2021-01-22 23:09:56 -0500

    fixing

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,37 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
+import { Server, IncomingMessage, ServerResponse } from 'http'
 
-createConnection().then(async connection => {
+const server: FastifyInstance = Fastify({})
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+const opts: RouteShorthandOptions = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          pong: {
+            type: 'string'
+          }
+        }
+      }
+    }
+  }
+}
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+server.get('/ping', opts, async (request, reply) => {
+  return { pong: 'it worked!' }
+})
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+const start = async () => {
+  try {
+    await server.listen(3000)
 
-}).catch(error => console.log(error));
+    const address = server.server.address()
+    const port = typeof address === 'string' ? address : address?.port
+
+  } catch (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
+}
+start()
